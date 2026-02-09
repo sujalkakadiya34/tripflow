de1275861740eea3f32d9ccaa11110978248bf7a
Sujal Kakadiya <sujalkakadiya34@gmail.com>
2026-02-09 15:56:20 +0530

    Fixed ClientTestimonials responsive issue

--- src/ClientTestimonials.jsx
+++ src/ClientTestimonials.jsx
@@ -38,8 +38,7 @@ const ClientTestimonials = () => {
               <img src={item.logo} alt="Company logo" />
             </div>
 
-            <p className="ct-quote">“{item.quote}”</p>
-
+            <p className="ct-quote">“{item.quote}”</p>  
             <div className="ct-author">
               <div className="ct-avatar">
                 <img src={item.avatar} alt={item.name} />
